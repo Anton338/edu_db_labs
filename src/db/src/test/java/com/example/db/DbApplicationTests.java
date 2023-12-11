b51a0278f982117f97defee45806977b2f366562
Anton Polishchuk <polishykav5@gmail.com>
2023-12-12 00:16:58 +0200

    fix

--- src/db/src/test/java/com/example/db/DbApplicationTests.java
+++ src/db/src/test/java/com/example/db/DbApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.db;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
