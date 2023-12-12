1ba660c4dda00731e08eda6444f6948692292690
Anton Polishchuk <polishykav5@gmail.com>
2023-12-12 13:53:08 +0200

    fix

--- src/db/src/main/java/com/example/db/model/User.java
+++ src/db/src/main/java/com/example/db/model/User.java
@@ -14,7 +14,7 @@ public class User {
 
   //  @Getter
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String username;
     private String password;
